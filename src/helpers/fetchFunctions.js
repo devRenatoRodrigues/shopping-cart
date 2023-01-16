b9b8305e3568f1dad4450f3026314b9821167c4c
devRenatoRodrigues <rehrlz@hotmail.com>
2023-01-16 18:28:54 -0300

    Requeriment 3 complete

--- src/helpers/fetchFunctions.js
+++ src/helpers/fetchFunctions.js
@@ -3,16 +3,14 @@ export const fetchProduct = () => {
 };
 
 export const fetchProductsList = async (QUERY) => {
-  if (!QUERY) throw new Error('Termo de busca não informado')
+  if (!QUERY) throw new Error('Termo de busca não informado');
   try {
     const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${QUERY}`);
     const data = await response.json();
-    return data.results
+    return data.results;
   } catch (error) {
-    return error.message
+    return error.message;
   }
-
 };
-
 
 console.log(fetchProductsList('computador'));
--- src/main.js
+++ src/main.js
@@ -1,8 +1,22 @@
 import { searchCep } from './helpers/cepFunctions';
 import './style.css';
 import { fetchProductsList } from './helpers/fetchFunctions';
+import { createProductElement } from './helpers/shopFunctions';
+
 document.querySelector('.cep-button').addEventListener('click', searchCep);
 
-console.log(fetchProductsList('mouse'));
+const products = document.querySelector('.products');
 
-logogog
+const createElements = async () => {
+  const productsArr = await fetchProductsList('computador');
+
+  productsArr.forEach((element) => {
+    const productElement = createProductElement(element);
+    products.appendChild(productElement);
+  });
+  // for (const product of productsArr) {
+  //     return console.log(product);
+  // }
+};
+
+console.log(createElements());
