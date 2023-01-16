c09cde18016bf55c97bda21ca07553620a56e3bc
devRenatoRodrigues <rehrlz@hotmail.com>
2023-01-16 15:28:15 -0300

    Requeriment  1 complete

--- src/helpers/fetchFunctions.js
+++ src/helpers/fetchFunctions.js
@@ -2,6 +2,11 @@ export const fetchProduct = () => {
   // seu código aqui
 };
 
-export const fetchProductsList = () => {
-  // seu código aqui
+export const fetchProductsList = async () => {
+  try {
+    const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${QUERY}`)
+    const data = await response.json()
+    console.log(data);
+  }
+
 };
