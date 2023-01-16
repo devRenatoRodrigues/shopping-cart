5a77cd00c003ceac5bf09ff2fe04f055b24d3c71
devRenatoRodrigues <rehrlz@hotmail.com>
2023-01-16 20:00:15 -0300

    Requeriment 4 complete

--- src/main.js
+++ src/main.js
@@ -5,18 +5,23 @@ import { createProductElement } from './helpers/shopFunctions';
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
 
-const products = document.querySelector('.products');
+const products = document.querySelector('.products')
 
 const createElements = async () => {
-  const productsArr = await fetchProductsList('computador');
+    loading()
+    const productsArr = await fetchProductsList('computador')
+    productsArr.forEach(element => {
+        const productElement = createProductElement(element)
+        products.appendChild(productElement)
+    });
+    const removeElement = document.querySelector('.loading')
+    removeElement.parentNode.removeChild(removeElement)
+}
 
-  productsArr.forEach((element) => {
-    const productElement = createProductElement(element);
-    products.appendChild(productElement);
-  });
-  // for (const product of productsArr) {
-  //     return console.log(product);
-  // }
-};
-
-console.log(createElements());
+const loading = () => {
+const newElement = document.createElement('h1');
+newElement.innerText = 'Carregando...'
+newElement.classList.add('loading')
+products.appendChild(newElement)
+}
+createElements()
