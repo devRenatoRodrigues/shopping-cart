cec4fc337132725bc16200ce18ae01f1e28343d2
devRenatoRodrigues <rehrlz@hotmail.com>
2023-01-17 18:53:49 -0300

    Requeriment 6 complete ?

--- src/tests/fetchProduct.test.js
+++ src/tests/fetchProduct.test.js
@@ -4,7 +4,25 @@ import product from './mocks/product';
 
 // implemente seus testes aqui
 describe('Teste a função fetchProduct', () => {
-  it('...', () => {
-    
+  it('testa se fetch product e uma função', () => {
+    const expected = typeof fetchProduct;
+    expect(expected).toBe('function');
+  });
+  it('testa se a função `fecthProduct` foi chamada', async () => {
+    await fetchProduct('MLB1405519561');
+    expect(fetch).toHaveBeenCalled();
+  });
+  it('testa se a fetch foi chamada com o endpoint correto ao passar o argumento `MLB1405519561`', async () => {
+    await fetchProduct('MLB1405519561');
+    expect(fetch).toHaveBeenCalledWith('https://api.mercadolibre.com/items/MLB1405519561');
+  });
+  it('testa se fetch product e uma função', async () => {
+    const expected = await fetchProduct('computador')
+    expect(expected).toEqual(product);
+  });
+  it('testa se fetch product e uma função', async () => {
+    const emptyProduct = '';
+
+    await expect(fetchProduct(emptyProduct)).rejects.toThrow(new Error('ID não informado'));
   });
 });
