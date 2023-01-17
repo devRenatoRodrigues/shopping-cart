f051b9c2e3e9627a91530c4dcef7a6e2fd6aca87
devRenatoRodrigues <rehrlz@hotmail.com>
2023-01-17 18:37:28 -0300

    Requeriment 5 complete

--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ document.querySelector('.cep-button').addEventListener('click', searchCep);
 
 const products = document.querySelector('.products');
 
+// função para criar um elemento o o texto 'carregando...'
 const loading = () => {
   const newElement = document.createElement('h1');
   newElement.innerText = 'Carregando...';
@@ -14,15 +15,32 @@ const loading = () => {
   products.appendChild(newElement);
 };
 
+// função para lançar o erro na tela.
+const displayError = () => {
+  const errorElement = document.createElement('h1');
+  errorElement.innerText = 'Algum erro ocorreu, recarregue a página e tente novamente';
+  errorElement.classList.add('error');
+  products.appendChild(errorElement);
+};
+
+// função que exibi 'carregando...' e o remove após adicionar elementos na tela.
 const createElements = async () => {
   loading();
-  const productsArr = await fetchProductsList('computador');
-  productsArr.forEach((element) => {
-    const productElement = createProductElement(element);
-    products.appendChild(productElement);
-  });
   const removeElement = document.querySelector('.loading');
-  removeElement.parentNode.removeChild(removeElement);
+  await fetchProductsList('computador')
+    .then((response) => {
+      response.forEach((element) => {
+        const productElement = createProductElement(element);
+        products.appendChild(productElement);
+      });
+
+      removeElement.parentNode.removeChild(removeElement);
+    })
+    // captura o erro e lança na tela.
+    .catch(() => {
+      removeElement.parentNode.removeChild(removeElement);
+      return displayError();
+    });
 };
 
 createElements();
